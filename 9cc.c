3ffa36a75a8b0d5c3ae1f687af45d7e6b03ad8bd
Wataru NAKAI <watnk0806@gmail.com>
2018-11-30 19:06:16 +0900

    再帰下降構文解析 の導入

--- 9cc.c
+++ 9cc.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
 
 enum
 {
@@ -55,10 +56,102 @@ void tokenize(char *p)
   tokens[i].input = p;
 }
 
-void error(int i)
+// 再帰下降構文解析
+enum
 {
-  fprintf(stderr, "予期せぬトークンです: %s\n", tokens[i].input);
+  ND_NUM = 256,
+};
+
+typedef struct Node
+{
+  int ty;
+  struct Node *lhs;
+  struct Node *rhs;
+  int val;
+} Node;
+
+int pos = 0;
+
+Node *new_node(int ty, Node *lhs, Node *rhs)
+{
+  Node *node = malloc(sizeof(Node));
+  node->ty = ty;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val)
+{
+  Node *node = malloc(sizeof(Node));
+  node->ty = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+void error(char *fmt, ...)
+{
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
   exit(1);
+}
+
+Node *number()
+{
+  if (tokens[pos].ty == TK_NUM)
+  {
+    return new_node_num(tokens[pos++].val);
+  }
+  error("数値ではないトークンです: %s\n", tokens[pos].input);
+}
+
+Node *expr()
+{
+  Node *lhs = number();
+  while (tokens[pos].ty != TK_EOF)
+  {
+    if (tokens[pos].ty == '+')
+    {
+      pos++;
+      return new_node('+', lhs, expr());
+    }
+    if (tokens[pos].ty == '-')
+    {
+      pos++;
+      return new_node('-', lhs, expr());
+    }
+    break;
+  }
+  return lhs;
+}
+
+// コード生成
+void gen(Node *node)
+{
+  if (node->ty == ND_NUM)
+  {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->ty)
+  {
+  case '+':
+    printf("  add rax, rdi\n");
+    break;
+  case '-':
+    printf("  sub rax, rdi\n");
+    break;
+  }
+  printf("  push rax\n");
 }
 
 int main(int argc, char **argv)
@@ -70,48 +163,15 @@ int main(int argc, char **argv)
   }
 
   tokenize(argv[1]);
+  Node *node = expr();
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
 
-  if (tokens[0].ty != TK_NUM)
-  {
-    error(0);
-  }
+  gen(node);
 
-  printf("  mov rax, %d\n", tokens[0].val);
-
-  int i = 1;
-  while (tokens[i].ty != TK_EOF)
-  {
-    if (tokens[i].ty == '+')
-    {
-      i++;
-      if (tokens[i].ty != TK_NUM)
-      {
-        error(i);
-      }
-      printf("  add rax, %d\n", tokens[i].val);
-      i++;
-      continue;
-    }
-
-    if (tokens[i].ty == '-')
-    {
-      i++;
-      if (tokens[i].ty != TK_NUM)
-      {
-        error(i);
-      }
-      printf("  sub rax, %d\n", tokens[i].val);
-      i++;
-      continue;
-    }
-
-    error(i);
-  }
-
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
